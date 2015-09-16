a587ea50e72034f45ca1d8d3889ec09d40fc3648
Ronny <ronny2205@gmail.com>
2015-09-16 01:16:59 +0000

    building a form for new places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,8 @@ class PlacesController < ApplicationController
 		#@places = Place.all
 		 @places = Place.paginate(:page => params[:page], :per_page => 4)
 	end	
+
+	def new
+		@place = Place.new
+	end
 end
