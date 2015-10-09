561853f221a1695ab690a0131d7d6008d0899368
Ronny <ronny2205@gmail.com>
2015-10-09 15:32:37 +0000

    user dashboard test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 
 	def show
-		@user = User.find(params[:id])
+		#@user = User.find(params[:id])
+		@user = User.where(:id => params[:id]).first
+		if @user.blank?
+      		render :text => "404 Not Found", :status => :not_found
+    	end
 	end	
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user show page" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    get :show, :id => user.id
+    assert_response :success
+  end
+
+  test "user show page, not found" do
+    get :show, :id => 'Ein'
+    assert_response :not_found
+  end
 end
